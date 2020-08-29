52d5d3d31c74d89c3c7083ea5f908bc7f4ab4793
Elsis Sitati <sitatielsis@gmail.com>
2020-08-29 20:01:41 +0300

    define entry point for the application

--- etl/src/main.py
+++ etl/src/main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+from schema import create_events_table
+from events import consume_events
+
+import time
+import os
+
+EVENTS_DIR = os.getenv('EVENTS_DIR', './events')
+
+EVENT_TYPES = ['cards', 'users']
+
+if __name__ == "__main__":
+    create_events_table()
+    while True:
+        for event_type in EVENT_TYPES:
+            consume_events(event_type=event_type, events_dir=EVENTS_DIR)
+        time.sleep(10)
